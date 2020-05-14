728bc3d571d0ab5d42d9c1b008271718c60004b1
Damjan <big.wide.blue@gmail.com>
2020-05-14 12:12:12 +0200

    feat: run kubernetes

---
+++ project-ml-microservice-kubernetes/make_prediction_k8s.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# if the service is exposed as NodePort the port is not known in advance.
+PORT=<service node port>
+HOSTEIP=<hoste ip>
+echo "Port: $PORT"
+echo "Hoste IP: $HOSTEIP"
+
+# POST method predict
+curl -d '{  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://$HOSTEIP:$PORT/predict
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,30 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath="wideblue/udacity-microservice"
+ 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run  udacity-app --image=$dockerpath --port 80 --restart Never
 
 # Step 3:
 # List kubernetes pods
+kubectl get po -o wide
 
 # Step 4:
 # Forward the container port to a host
+# note this would usually fail because the 
+# udacity-app pod would not start yet
+# sleep 60
+kubectl port-forward udacity-app 8000:80
+
+# Standard way to expose service on host node,
+# the exposed port number is set by cluster
+# kubectl expose po udacity-app --type NodePort
+
+# This would expose service to a specific port on the host 
+# The range of valid ports is 30000-32767
+# kubectl create service nodeport udacity-app-svc --tcp=80:80 \
+# --node-port=30080 -o yaml --dry-run=client | kubectl set \
+# selector --local -f - 'run=udacity-app' -o yaml | kubectl create -f -
 
